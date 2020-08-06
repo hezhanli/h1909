143ba0cf43231796d4abd3c56664d667d84c617e
hzl <1724042825@qq.com>
2020-08-06 16:49:13 +0800

    作业提交

---
+++ day06/ssmday06/src/main/java/com/xiaoshu/dao/EmpMapper.java
@@ -0,0 +1,24 @@
+package com.xiaoshu.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.xiaoshu.base.dao.BaseMapper;
+import com.xiaoshu.entity.Emp;
+import com.xiaoshu.entity.EmpExample;
+import com.xiaoshu.entity.EmpVo;
+
+public interface EmpMapper extends BaseMapper<Emp> {
+    long countByExample(EmpExample example);
+
+    int deleteByExample(EmpExample example);
+
+    List<Emp> selectByExample(EmpExample example);
+
+    int updateByExampleSelective(@Param("record") Emp record, @Param("example") EmpExample example);
+
+    int updateByExample(@Param("record") Emp record, @Param("example") EmpExample example);
+
+	List<EmpVo> findPage(EmpVo emp);
+}
