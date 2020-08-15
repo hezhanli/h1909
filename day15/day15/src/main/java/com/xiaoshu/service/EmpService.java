0157c99a6511c43e982c5e567d8c53bc97ab9fd6
hzl <1724042825@qq.com>
2020-08-15 21:46:09 +0800

    作业提交

---
+++ day15/day15/src/main/java/com/xiaoshu/service/EmpService.java
@@ -0,0 +1,70 @@
+package com.xiaoshu.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.github.pagehelper.util.StringUtil;
+import com.xiaoshu.dao.DeptMapper;
+import com.xiaoshu.dao.EmpMapper;
+import com.xiaoshu.dao.UserMapper;
+import com.xiaoshu.entity.Dept;
+import com.xiaoshu.entity.Emp;
+import com.xiaoshu.entity.EmpVo;
+import com.xiaoshu.entity.User;
+import com.xiaoshu.entity.UserExample;
+import com.xiaoshu.entity.UserExample.Criteria;
+
+@Service
+public class EmpService {
+
+	@Autowired
+	private EmpMapper empMapper;
+
+	/*// 新增
+	public void addUser(User t) throws Exception {
+		userMapper.insert(t);
+	};
+
+	// 修改
+	public void updateUser(User t) throws Exception {
+		userMapper.updateByPrimaryKeySelective(t);
+	};
+
+	// 删除
+	public void deleteUser(Integer id) throws Exception {
+		userMapper.deleteByPrimaryKey(id);
+	};*/
+
+	
+
+	/*// 通过用户名判断是否存在，（新增时不能重名）
+	public User existUserWithUserName(String userName) throws Exception {
+		UserExample example = new UserExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andUsernameEqualTo(userName);
+		List<User> userList = userMapper.selectByExample(example);
+		return userList.isEmpty()?null:userList.get(0);
+	};
+*/
+	public PageInfo<EmpVo> findUserPage(EmpVo empVo, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		
+		List<EmpVo> list = empMapper.findPage(empVo);
+		
+		PageInfo<EmpVo> pageInfo = new PageInfo<EmpVo>(list);
+		return pageInfo;
+	}
+	//
+	@Autowired()
+	private DeptMapper deptMapper;
+	public List<Dept> findDeptList() {
+		return deptMapper.selectAll();
+	}
+
+
+}
